b41e7981e17854e1e35ece3cc59b806329e446c4
Vaal34 <5619@holbertonstudents.com>
2024-04-15 13:10:59 +0200

    create function that print teachers

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -10,3 +10,13 @@ export interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;
 }
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = function(firstName, lastName){
+    return `${firstName.charAt(1)}. ${lastName}`
+}
+
+console.log(printTeacher("Melia", "Valentin"))
