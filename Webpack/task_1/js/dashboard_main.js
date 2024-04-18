48919b59b6555b1372ce174bc3b4c6d2ba8f15bd
Vaal34 <5619@holbertonstudents.com>
2024-04-18 21:05:58 +0200

    earning how to use Webpack with a config file

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from "jquery";
+import _ from "lodash";
+
+$(function(){
+    $("body").append(
+        $("<p>").text("Holberton Dashboard"),
+        $("<p>").text("Dashboard data for the students"),
+        $("<button>").text("Click here to get started"),
+        $("<p id='count'></p>"),
+        $("<p>").text("Copyright - Holberton School")
+    );
+});
+
+const count = 0;
+function updateCounter(){
+    count++;
+    $("#count").text(`${count} click on the button`);
+};   
+$("body button").on("click", _.debounce(updateCounter(), 500))
