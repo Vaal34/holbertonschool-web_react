34141b549d362e5a5daf4d05219f9bedd8a11f6d
Vaal34 <5619@holbertonstudents.com>
2024-04-24 13:04:00 +0200

    install enzyme

---
+++ react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { getFullYear, getFooterCopy } from "./utils"
+
+function App() {
+
+  const isIndex = true
+
+  const currentYear = getFullYear();
+  const footerText = getFooterCopy(isIndex);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={require("./holberton-logo.jpg")} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label for="email">Email:</label>
+        <input type="email" id="email"></input>
+        <label for="password">Password:</label>
+        <input type="password" id="password"></input>
+        <button>OK</button>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {currentYear} - {footerText}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
