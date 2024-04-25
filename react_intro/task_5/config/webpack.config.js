63c2f01c748b9f26abf4a94aaf0dc21bf3157304
Vaal34 <5619@holbertonstudents.com>
2024-04-25 15:50:27 +0200

    webpack

---
+++ react_intro/task_5/config/webpack.config.js
@@ -0,0 +1,42 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true,
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$|jsx/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+};
---
+++ react_intro/task_5/src/Notifications/Notifications.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import "./Notifications.css"
+import { getLatestNotification } from '../utils'; 
+
+const closePng = require('./close-icon.png');
+
+const Notifications = () => {
+
+    const closeClick = () => {
+        console.log("Close button has been clicked");
+    };
+
+    return(
+        <div className='Notifications'>
+            <button
+            onClick={closeClick}
+            aria-label="Dismiss"
+            style={{ 
+                cursor: 'pointer',
+                backgroundColor: 'transparent', 
+                border: 'none',
+                float: 'right',
+            }}>
+                <img src={closePng} alt="close" width={10}></img>
+            </button>
+            <p>Here is the list of notifications</p>
+            <ul>
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li data-priority="urgent" dangerouslySetInnerHTML={{__html: getLatestNotification()}}></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Notifications
