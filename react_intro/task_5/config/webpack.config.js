15f5345cad174fe9d03a9d1102037c6509531212
Vaal34 <5619@holbertonstudents.com>
2024-04-25 15:51:24 +0200

    push webpack

--- react_intro/task_5/config/webpack.config.js
+++ react_intro/task_5/config/webpack.config.js
@@ -1,42 +1,43 @@
-// webpack.config.js
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve("./dist"),
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
     hot: true,
+    contentBase: path.resolve("./dist"),
+    compress: true,
+    port: 8564,
   },
-  mode: "production",
   module: {
     rules: [
       {
-        test: /\.js$|jsx/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+        loader: "babel-loader",
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
+          "file-loader",
           {
-            loader: 'file-loader',
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
           },
         ],
       },
     ],
   },
-  devtool: 'inline-source-map',
 };
