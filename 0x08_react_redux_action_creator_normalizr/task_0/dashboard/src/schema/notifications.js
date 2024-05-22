85aaa0412fc565989c8aef477115e282e81f6f45
Vaal34 <5619@holbertonstudents.com>
2024-05-22 21:14:51 +0200

    create schema normalizr an test

--- 0x08_react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,5 +1,8 @@
 const dataNotif = require('../../../../notification.json');
 
 export function getAllNotificationsByUser(userId){
-    return dataNotif.filter(notification => notification.author.id === userId);
-}
+    const userContext = dataNotif
+        .filter(notification => notification.author.id === userId)
+        .map(notification => notification.context);
+    return userContext
+};
